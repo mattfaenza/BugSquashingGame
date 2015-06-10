9f59a284573d5a54b3bd39334e6b2f3277c518a4
RyanFoxMDFung <ryanshiuhong.fung@mail.utoronto.ca>
2015-06-10 01:41:21 -0400

    added css screen change and timer stuff
    The start screen can progress to the game screen now, and starts a
    countdown. Pause and resume buttons are implemented that stop and resume
    the timer. We can use those functions in the JS to do their respective
    actions for the whole game. Theyre placed in an ugly manner atm, that's
    all.

--- JScript.js
+++ JScript.js
@@ -1,3 +1,36 @@
+var seconds = 60;
+var countD = null;
+
+//function to change the screen from start to game screen
 function changeScreens(){
-	alert('Hiya!');
+	document.getElementById('startScreen').style.display='none';
+	document.getElementById('infoBar').style.display='block';
+	document.getElementById('gameScreen').style.display='block';
+	timerCount();
+}
+
+//starts the timer and recursively countsdown
+function timerCount(){
+	document.getElementById("countDown").innerHTML = "Time Left:" + seconds;
+	if(seconds == 0){
+		alert("Game Over!");
+	}else{
+		seconds--;
+		countD = setTimeout("timerCount()", 100);
+	}
+}
+
+//pauses the game
+function pause(){
+	clearTimeout(countD);
+	//gonna change the pause button to a play button
+	document.getElementById('pauseButton').style.display='none';
+	document.getElementById('resumeButton').style.display='block';
+}
+
+//resumes the game
+function resume(){
+	countD = setTimeout("timerCount()", 100);
+	document.getElementById('pauseButton').style.display='block';
+	document.getElementById('resumeButton').style.display='none';
 }
