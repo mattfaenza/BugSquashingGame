f950c5d6a7e6142161a7155543d94ae13aae96a9
RyanFoxMDFung <ryanshiuhong.fung@mail.utoronto.ca>
2015-06-12 02:15:56 -0400

    Added a bug spawner that spawns every 1 second
    will work on it more tommorow. I think bug movement would be defined in
    the bugspawn function. I'm a little worried about redirection of
    movement though. Like when a bug eats a food object, all bugs need to
    redirect its movement

--- JScript.js
+++ JScript.js
@@ -1,6 +1,7 @@
 var seconds = 60;
 var countD;
 var foodBits = [];
+var swarm = [];
 window.onload = startClick;
 var foodSpawnCount = 0;
 
@@ -21,6 +22,7 @@ function changeScreens(){
 	spawnFood();
 	foodSpawnCount++;
 	}while(foodSpawnCount != 5)
+	setInterval(function(){spawnBug()}, 1000);
 	timerCount();
 }
 
@@ -31,7 +33,7 @@ function timerCount(){
 		alert("Game Over!");
 	}else{
 		seconds--;
-		countD = setTimeout("timerCount()", 100);
+		countD = setTimeout("timerCount()", 1000);
 	}
 }
 
@@ -45,14 +47,14 @@ function pause(){
 
 //resumes the game
 function resume(){
-	countD = setTimeout("timerCount()", 100);
+	countD = setTimeout("timerCount()", 1000);
 	document.getElementById('pauseButton').style.display='block';
 	document.getElementById('resumeButton').style.display='none';
 }
 
 //spawns a single food object
 function spawnFood(){
-	var foodNode = document.createElement("p");
+	var foodNode = document.createElement("food");
 	var gameStage = document.getElementById("gameScreen");
 	var ctx = gameStage.getContext("2d");
 	ctx.fillStyle = "#FF0000";
@@ -66,4 +68,27 @@ function spawnFood(){
 	foodBits.push(foodNode);
 	gameStage.appendChild(foodNode);
 }
+//spawns a single bug object
+function spawnBug(){
+	var bugNode = document.createElement("bug");
+	var gameStage = document.getElementById("gameScreen");
+	var ctx = gameStage.getContext("2d");
+	ctx.fillStyle = "Blue";
+	
+	bugNode.x = (Math.random()*(gameStage.width - 40)) + 15;
+	ctx.fillRect(bugNode.x, 0, 20, 20);
+	
+	swarm.push(bugNode);
+	gameStage.appendChild(bugNode);
+}
+
+
+
+
+
+
+
+
+
+
 
